42bf2e969e910028a36d490a19002df98dab43d9
Mikhail Popov <mpopov@wikimedia.org>
2017-05-17 23:42:51 +0200

    Add example of multiple neuralnet forecasts + update R enwiki pageviews

--- R/data.R
+++ R/data.R
@@ -5,8 +5,7 @@
 #' r_enwiki <- pageviews::article_pageviews(
 #'   project = "en.wikipedia", article = "R (programming language)",
 #'   platform = "desktop", user_type = "user",
-#'   start = "2015100100", end = "2017013000"
-#' )[, c("date", "views")]
+#'   end = "2017051600")[, c("date", "views")]
 #' names(r_enwiki) <- c("date", "pageviews")
 #' }
 #' @format A \code{data.frame} with columns "date" and "pageviews"
